a9d7a71bc4d1aad9b569bb53da8541229bfec2a4
yesky93 <huming0618@gmail.com>
2017-12-26 07:46:20 +0000

    init day01

---
+++ day01/flow.js
@@ -0,0 +1,19 @@
+const Rx = require('rxjs/Rx');
+
+const observable = Rx.Observable.create(function (observer) {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  setTimeout(() => {
+    observer.next(4);
+    observer.complete();
+  }, 1000);
+});
+
+console.log('> just before subscribe');
+observable.subscribe({
+  next: x => console.log('got value ' + x),
+  error: err => console.error('something wrong occurred: ' + err),
+  complete: () => console.log('done'),
+});
+console.log('> just after subscribe');
